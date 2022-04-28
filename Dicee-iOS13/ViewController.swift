8861813bc8af9f8dffca8f261ecf43ddf320b008
elahe <el.sh192014@gmail.com>
2022-04-28 16:50:39 +0200

    pass random array element to diceImageView 1 and 2

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController {
     }
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
-       
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
     }
     
 }
